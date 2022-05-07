bd1edc677ac1076c6c08a7986a04736c246dd162
Nickbenve <n.benvenaste@frba.utn.edu.ar>
2022-05-07 06:29:32 -0300

    arreglo

---
+++ link/src/main/java/controler/OrdenController.java
@@ -0,0 +1,10 @@
+package controler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ordenes")
+public class OrdenController {
+
+}
