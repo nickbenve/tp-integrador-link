dc74efc3027ef50c78de117c3f796be9e8cb7433
Nickbenve <n.benvenaste@frba.utn.edu.ar>
2022-06-07 00:07:33 -0300

    hecho con dto

--- link/src/main/java/app/repo/compl/RepoVendedorCompl.java
+++ link/src/main/java/app/repo/compl/RepoVendedorCompl.java
@@ -38,17 +38,29 @@ public class RepoVendedorCompl {
 	
 	@Transactional
 	@RequestMapping(method = RequestMethod.POST,value="/vendedores/{vendedorId}/crearProducto")
-	public @ResponseBody String crearProducto(@PathVariable("vendedorId") Integer vendedorId,@RequestBody Producto producto){
+	public @ResponseBody String crearProducto(@PathVariable("vendedorId") Integer vendedorId,@RequestBody ProductoINPUT productoIN){
 		
+
 		Optional<CotizacionDolar> cotizacion=repoDolar.findById(1);
 		
-		CotizacionDolar dolar= cotizacion.get();
+		CotizacionDolar dolar= new CotizacionDolar();
+		
+		Producto producto= new Producto();
+		this.machear(producto,productoIN);
+
+		
+		Optional<Proveedor> opcionalProveedor=repoProv.findById(productoIN.getId_proveedor());
+		if(opcionalProveedor.isEmpty()) {
+			return "no existe un proveedor con ese id";
+		}
+		producto.setProveedor(opcionalProveedor.get());
+		
+		
 		Optional<Vendedor> opcionalVendedor=repoVen.findById(vendedorId);
 		if(opcionalVendedor.isEmpty()) {
 			return "vendedor no encontrado";
 		}
 		
-		Optional<Proveedor> opcionalProveedor=repoProv.findById(producto.getProveedor().getId());
 		
 		if(opcionalProveedor.isEmpty()) {
 			return "proveedor no encontrado";
@@ -64,5 +76,16 @@ public class RepoVendedorCompl {
 				repoProductos.save(producto);
 				return "se guardo el producto";				
 		}
+	}
+
+
+
+	private void machear(Producto producto, ProductoINPUT productoIN) {
+		producto.setDescripcion(productoIN.getDescripcion());
+		producto.setEsPesos(productoIN.getEsPesos());
+		producto.setNombre(productoIN.getNombre());
+		producto.setPrecio(productoIN.getPrecio());
+		producto.setStock(productoIN.getStock());
+
 	}
 }
