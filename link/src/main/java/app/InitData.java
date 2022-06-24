017d0e78e9c5dd2b14b1bc288febbb7b031a55b2
Nickbenve <n.benvenaste@frba.utn.edu.ar>
2022-06-24 14:11:53 -0300

    finalizado

--- link/src/main/java/app/InitData.java
+++ link/src/main/java/app/InitData.java
@@ -78,31 +78,40 @@ public class InitData implements CommandLineRunner  {
 			
 			
 			
-			Proveedor proveedor1=new Proveedor("dia","aaaaaa");
-			Proveedor proveedor2=new Proveedor("diarco","bbbbb");
+			Proveedor proveedor1=new Proveedor("Dia","aaaaaa");
+			Proveedor proveedor2=new Proveedor("Diarco","bbbbb");
 			repoProveedores.save(proveedor1);
 			repoProveedores.save(proveedor2);
 			
 			
-			Producto producto1=new Producto("Remera" , "Color negro",false,"https://sublitextil.com.ar/wp-content/uploads/2019/01/Remera-sublimar-hombre-.jpg",200.0,5000,proveedor1);
-			Producto producto2=new Producto("Pantalon negro","talle m",false,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpe-lszpHSJetmVqGxvlN0RX0SiWhS4Fyc9w&usqp=CAU",1.0,5000,proveedor2);
-			List<Producto> productosIniciales=List.of(producto1,producto2);
+			Producto producto1=new Producto("Remera" , "Color negro",true,"https://sublitextil.com.ar/wp-content/uploads/2019/01/Remera-sublimar-hombre-.jpg",200.0,5000,proveedor1);
+			Producto producto2=new Producto("Pantalon negro","Talle m",true,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpe-lszpHSJetmVqGxvlN0RX0SiWhS4Fyc9w&usqp=CAU",1.0,5000,proveedor2);
+			Producto producto3=new Producto("Heladera ","Alta",false,"https://www.lg.com/ar/images/heladeras/md06176357/gallery/1100_1.jpg",2000.0,5000,proveedor1);
+			Producto producto4=new Producto("Auto","Alta gama",false,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpe-lszpHSJetmVqGxvlN0RX0SiWhS4Fyc9w&usqp=CAU",1.0,5000,proveedor2);
+			Producto producto5=new Producto("Mesa" , "Madera",true,"https://i.ytimg.com/vi/d_zHVhzzPI4/maxresdefault.jpg",300.0,5000,proveedor1);
+			Producto producto6=new Producto("Televisor","40 pulgadas",false,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSGf03zoJZSZ-r2wZs_PI1v4aTacBIX-6VdQ&usqp=CAU",1.0,5000,proveedor2);
+			Producto producto7=new Producto("Pc" , "16 de ram",true,"https://sublitextil.com.ar/wp-content/uploads/2019/01/Remera-sublimar-hombre-.jpg",200.0,5000,proveedor1);
+			Producto producto8=new Producto("Jean","azul",true,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpe-lszpHSJetmVqGxvlN0RX0SiWhS4Fyc9w&usqp=CAU",1.0,5000,proveedor2);
+			
+			List<Producto> productosIniciales=List.of(producto1,producto2,producto1,producto3,producto4,producto5,producto6,producto7,producto8);
 			productosIniciales.stream().forEach(producto -> {
 			repoProd.save(producto);
 			});
 			
 
 			
-			Vendedor vendedor1= new Vendedor("Carlos",11111,Arrays.asList(proveedor1),Arrays.asList(producto1,producto2));
-
+			Vendedor vendedor1= new Vendedor("Carlos",11111,Arrays.asList(proveedor1),Arrays.asList(producto1,producto2,producto3,producto4));
+			Vendedor vendedor2= new Vendedor("Juan",12222,Arrays.asList(proveedor2),Arrays.asList(producto5,producto6,producto7,producto8));
 			repoVendedores.save(vendedor1);
-
+			repoVendedores.save(vendedor2);
+		
 			Persona persona1=new Persona("v","v",vendedor1);
+			Persona persona2=new Persona("v","v",vendedor2);
 			Persona persona3=new Persona("c","c",cliente1);
 			Persona persona4=new Persona("c2","c2",cliente2);
 			
 			repoPersonas.save(persona1);
-	
+			repoPersonas.save(persona2);
 			repoPersonas.save(persona3);
 			repoPersonas.save(persona4);
 			
@@ -122,7 +131,7 @@ public class InitData implements CommandLineRunner  {
 			
 			repoMembrecia.save(membrecia);
 			
-			DescuentoMembrecia desmembre=new DescuentoMembrecia(membrecia,0.25);
+			DescuentoMembrecia desmembre=new DescuentoMembrecia(membrecia,0.5);
 			carritoc2.agregarPromo(desmembre);		
 			List<Promocion> promocionesIniciales=List.of(promoMedioDePago,cuponProveedor,desmembre);
 			promocionesIniciales.stream().forEach(promocion -> {
@@ -132,6 +141,7 @@ public class InitData implements CommandLineRunner  {
 			
 			carritoc1.agregarPromo(promoMedioDePago);
 			carritoc2.agregarPromo(promoMedioDePago);
+
 			repoCarrito.save(carritoc1);
 			repoCarrito.save(carritoc2);
 			
--- link/src/main/java/app/repositorios/VendedorComplement.java
+++ link/src/main/java/app/repositorios/VendedorComplement.java
@@ -1,11 +1,15 @@
 package app.repositorios;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -58,4 +62,31 @@ public class VendedorComplement {
 		
 		
 	}
+	@Transactional
+	@RequestMapping(method = RequestMethod.GET,value="/{vendedorID}/productos/page")
+	public @ResponseBody Page<Producto> get(@PathVariable("vendedorID") UUID vendedorID,Pageable pageable) {
+		
+		Optional<Vendedor> vendedorOp=repoVen.findById(vendedorID);
+		
+		Vendedor vendedor = vendedorOp.get();
+		
+
+		
+		final int start = (int)pageable.getOffset();
+		final int end = Math.min((start + pageable.getPageSize()), vendedor.getProductos().size());
+		if(start<end) {
+			final Page<Producto> page = new PageImpl<>(vendedor.getProductos().subList(start, end), pageable,vendedor.getProductos().size());
+			return page;
+		}
+		else {
+			final Page<Producto> page = new PageImpl<>(List.of(), pageable,vendedor.getProductos().size());
+			return page;
+		}
+		
+
+		
+		
+		
+	}
+	
 }
